1a1dc006703ac9c9ad7d63f98da129555e0761d8
William Fisher <jarell@gmail.com>
2014-08-09 12:39:52 -0400

    first test

---
+++ js/bill.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.bill-resume-job').hover(
+      function(){
+      $(this).addClass('job-selected');
+    }, function(){
+      $(this).removeClass('job-selected');
+    });
+
+
+});
